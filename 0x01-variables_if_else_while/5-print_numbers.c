f2f3aae87d164f31840798bd02918ba3d8622add
SophieO1970 <sophieolondie@gmail.com>
2023-10-06 14:47:15 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10 from 0
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char singled;
+
+	for (singled = '0'; singled <= '9'; singled++)
+	{
+		putchar(singled);
+	}
+	printf("\n");
+
+	return (0);
+}
