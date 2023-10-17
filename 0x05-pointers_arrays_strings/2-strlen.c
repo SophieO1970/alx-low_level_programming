9bcb013ab227f99e7da3325938922c0f833485fc
SophieO1970 <sophieolondie@gmail.com>
2023-10-17 15:51:26 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to measure
+ *
+ *Return: length of s
+ */
+int _strlen(char *s)
+{
+
+
+
+
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
