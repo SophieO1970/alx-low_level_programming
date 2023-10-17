27861d89b281859796dcc6d78378b7bd047cb0e4
SophieO1970 <sophieolondie@gmail.com>
2023-10-17 16:38:00 -0700

    starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string in reverse
+ * @s: string to be reversed
+ *
+ * Return: reversed string
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	length--;
+
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}
