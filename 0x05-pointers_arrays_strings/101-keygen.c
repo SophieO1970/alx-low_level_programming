a341b560d0e098d691741e2d219ae865717a8d0e
SophieO1970 <sophieolondie@gmail.com>
2023-10-17 17:26:46 -0700

    random password and  convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random password
+ * for 101-crackme
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+	char password[65];
+	int target = 2772;
+	
+	srand((unsigned int)time(NULL));
+	
+	for (i = 0; i < 64; i++)
+	{
+		password[i] = (rand() % 94) + 33;
+		
+		if ((sum + password[i]) > target)
+		{
+			password[i] = target - sum;
+			break;
+		}
+		sum += password[i];
+	}
+	password[64] = '\0';
+	printf("%s\n", password);
+	return 0;
+}
