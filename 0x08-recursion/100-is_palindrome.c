5152a792d70f8659ec8fe4dfc227d8c64ec8e3df
SophieO1970 <sophieolondie@gmail.com>
2023-10-26 05:27:59 -0700

    wildcamp

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * find_lindrome - looks for palindrome
+ * @start: beginning of string
+ * @end: end of string
+ *
+ * Return: 1 or 0
+ */
+int find_lindrome(char *start, char *end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (*start != *end)
+	{
+		return (0);
+	}
+
+	return (find_lindrome(start + 1, end - 1));
+}
+
+/**
+ * _strlen_recursion - finds length of
+ * string
+ * @s: string to measure
+ *
+ * Return: lenght of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + (_strlen_recursion(s + 1)));
+}
+/**
+ * is_palindrome - shows if s is a
+ * palindrome
+ * @s: string to check
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	if (len <= 1)
+	{
+		return (1);
+	}
+
+	return (find_lindrome(s, s + len - 1));
+}
