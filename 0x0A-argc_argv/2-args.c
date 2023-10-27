6b5508af9a7a495b049c845fe99ad4f93583339d
SophieO1970 <sophieolondie@gmail.com>
2023-10-27 14:59:39 -0700

    numbers in coins and positive numbers

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments received
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	for (; argc > 0; argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+
+	return(0);
+}
