a14674fcd5b28283f1e8fef687385e200a6dea77
SophieO1970 <sophieolondie@gmail.com>
2023-11-06 15:52:27 -0800

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - points to file it was
+ * compiled from
+ * @void: empty
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
