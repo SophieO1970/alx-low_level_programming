dff339adadfeab8611034b060ff496532dd8fe65
SophieO1970 <sophieolondie@gmail.com>
2023-11-19 10:30:46 -0800

    add the prototypes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: The pointer to the memory location
- * @b: The character to fill
- * @n: The number of bytes
+ * _memset - fills memory with
+ * constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes
  *
- * Return: The pointer of the memory location now updated/manipulated
+ * Return: Always (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *byte_ptr = s;
-	unsigned int i;
+	char *p = s;
 
-	b = 'b';
-	for (i = 0; i < n; i++)
+	while (n > 0)
 	{
-		byte_ptr[i] = b;
+		*p = b;
+		p++;
+		n--;
 	}
 
 	return (s);
